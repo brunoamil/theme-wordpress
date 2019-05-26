aa7f440cd022cc197f6a5114ab6c30ee11639c77
bruno lima <bruno-amil@hotmail.com>
2019-05-26 16:05:25 -0300

    chamando thumbnails e reset css

--- wp-content/themes/novotema/header.php
+++ wp-content/themes/novotema/header.php
@@ -1,8 +1,12 @@
 <!doctype html>
 <html>
     <head>
+        <!-- salvando a funcao directory -->
+        <?php $home = get_template_directory_uri(); ?>
         <meta charset="utf-8">
-        <link rel="stylsheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="<?= $home; ?>/reset.css">
+        <link rel="stylesheet" type="text/css" href="<?= $home; ?>/style.css">
+
         <!-- chamando o menu admin quando tiver logado -->
         <?php wp_head(); ?>
     </head>
