0b08b36fc2f5533fafe49bf09d40c6fe91e4ea2a
bruno lima <bruno-amil@hotmail.com>
2019-05-26 17:21:01 -0300

    criando a single.php para chamar na pagina principal

--- wp-content/themes/novotema/functions.php
+++ wp-content/themes/novotema/functions.php
@@ -3,4 +3,47 @@
 add_theme_support('post-thumbnails');
 
 
-?>
+
+//############################################
+//registrar conteudo novo - customizado, onde passa várias configuracoes com $args(array)
+//public  - para aparecer no menu 
+//labels - é o nome do titulo e outros do menu novo imoveis
+//add_new_item - titulo para adicionar novo imovel
+//edit_item - titulo para editar imovel
+//visitar o site https://codex.wordpress.org/Function_Reference/register_post_type e verificar os outros titulos
+//visitar icone wordpress https://developer.wordpress.org/resource/dashicons/#heart
+
+function cadastrando_post_type_imoveis() {
+        $nomeSingular = 'Imóvel';
+        $nomePlural = 'Imóveis';
+        $description = 'Imóveis da Mobiliaria Malura';
+
+        $labels = array(
+            'name' => $nomePlural,
+            'name_singular' => $nomeSingular,
+            'add_new_item' => 'Adicionar novo ' . $nomeSingular,
+            'edit_item' => 'Editar ' . $nomeSingular
+        );
+
+        $supports = array(
+            'title',
+            'editor',
+            'thumbnail'
+        );
+
+        $args = array(
+            'labels' => $labels,
+            'public' => true,
+            'description' => $description,
+            'menu_icon' => 'dashicons-admin-home',
+            'supports' => $supports
+        );
+
+        register_post_type('imovel', $args);
+
+}
+
+//chamando as funcoes que o wordpress deve chamar
+add_action('init', 'cadastrando_post_type_imoveis');
+
+//############################################
--- wp-content/themes/novotema/index.php
+++ wp-content/themes/novotema/index.php
@@ -4,20 +4,29 @@
 <main class="home-main">
     <div class="container">
     <h1> Bem vindo! :) </h1>
+
+        <!-- chamando meus post-imoveis -->
+        <?php 
+            $args = array('post_type' => 'imovel');
+            $loop = new WP_Query($args);
+        ?>
+
         <ul class="imoveis-listagem">
             <!-- chamando os post -->
             <?php 
-                if(have_posts()){
-                    while(have_posts()){
-                        the_post();
+                if($loop ->have_posts()){
+                    while($loop ->have_posts()){
+                        $loop->the_post();
             ?>
             <li class="imoveis-listagem-item">
+            <a href="<?php the_permalink(); ?>">
                 <!-- imagem em destaque -->
                 <?php the_post_thumbnail(); ?>
-                <!-- Titulo do post-->
-                <h2><?php the_title(); ?></h2>
-                <!-- conteudo do post-->
-                <p><?php the_content(); ?></p>
+                    <!-- Titulo do post-->
+                    <h2><?php the_title(); ?></h2>
+                    <!-- conteudo do post-->
+                    <p><?php the_content(); ?></p>
+                </a>
             </li>
             <?php
                     }
